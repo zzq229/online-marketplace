4461575290a36000ce8c2217e698bf9fea4cf68d
zzq229 <zzqwaterloo@gmail.com>
2019-01-16 16:52:47 -0500

    shopping cart operation

--- routes/cart.js
+++ routes/cart.js
@@ -29,30 +29,25 @@ router.post('/addItem', async function (req, res, next) {
   }
 })
 
-// router.post('/addItem', async function (req, res, next) {
-//   let items = req.body
-//   if (!items) return res.status(400).send('Not enough parameter')
-//   else {
-//     var promises = []
+router.get('/getcart', async function (req, res, next) {
+  let cart = await db.getAllCart()
+  let totalValue = 0
+  for (let item in cart) {
+    let product = await db.getOneProduct(cart[item].Title)
+    let price = parseFloat(product[0].Price)
+    totalValue += price * cart[item].Count
+  }
+  cart.push({ 'Total value': totalValue })
+  return res.status(200).send(cart)
+})
 
-//     for (let key in items) {
-//       let product = await db.getOneProduct(key)
-//       if (product.length === 0) return res.status(500).send(key + ' is not a good')
-
-//       let cartItemInDB = await db.getCartOneItem(key)
-
-//       if (cartItemInDB.length === 0) {
-//         if (product[0].Inventory_count < items[key]) return res.status(500).send('There is not enough ' + key)
-//       }
-//     }
-
-//     return Promise.all(promises).then(res => {
-//       console.log(res)
-//       res.status(200).send('Cart added')
-//     })
-//     // if (product[0].Inventory_count < countInDB + items[key]) return res.status(500).send('There is not enough ' + key)
-//     // ar.push({ 'Title': key, 'Count': items[key] })
-//   }
-// })
+router.post('/complete', async function (req, res, next) {
+  let cart = await db.getAllCart()
+  return db.updateCartItem(cart).then(_ => {
+    return db.deleteCart().then(_ => {
+      return res.status(200).send('Complete purchase')
+    })
+  })
+})
 
 module.exports = router
--- utilities/db.js
+++ utilities/db.js
@@ -39,9 +39,28 @@ async function addCartItem (items) {
   return Promise.all(promises)
 }
 
+async function updateCartItem (items) {
+  let db = await dbPromise
+  let promises = []
+  for (let i in items) {
+    promises.push(db('Product').where('Title', items[i].Title).decrement('Inventory_count', items[i].Count))
+  }
+  return Promise.all(promises)
+}
+
 async function getCartOneItem (title) {
   let db = await dbPromise
   return db('Cart').where('Title', title)
+}
+
+async function getAllCart () {
+  let db = await dbPromise
+  return db('Cart')
+}
+
+async function deleteCart () {
+  let db = await dbPromise
+  return db('Cart').del()
 }
 
 module.exports.createProduct = createProduct
@@ -49,4 +68,7 @@ module.exports.getOneProduct = getOneProduct
 module.exports.getAllProduct = getAllProduct
 module.exports.buyProduct = buyProduct
 module.exports.addCartItem = addCartItem
+module.exports.getAllCart = getAllCart
 module.exports.getCartOneItem = getCartOneItem
+module.exports.updateCartItem = updateCartItem
+module.exports.deleteCart = deleteCart
